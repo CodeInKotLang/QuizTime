238b727556cb538068d7ff0d8839fc600c100ca5
Mohammad Arif <codeinkotlang@gmail.com>
2025-03-01 22:35:33 +0530

    some changes in QuizScreen

--- app/src/main/java/com/synac/quiztime/presentation/quiz/QuizScreen.kt
+++ app/src/main/java/com/synac/quiztime/presentation/quiz/QuizScreen.kt
@@ -26,7 +26,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
 import com.synac.quiztime.domain.model.QuizQuestion
